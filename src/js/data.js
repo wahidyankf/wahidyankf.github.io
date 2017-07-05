50847dc3821f5377b11fd3c2778a48473c94648c
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2017-07-05 10:01:33 +0700

    create data template

---
+++ src/js/data.js
@@ -0,0 +1,70 @@
+var dataCV = {
+  biodata: {
+    name: "Wahidyan Kresna Fridayoka",
+    nickname: "Yoka",
+    summary: "summary here",
+  },
+  skills: {
+    technicalSkills: {
+      proficient: [],
+      fairlyProficient: [],
+      familiar: []
+    },
+    languageSkills: {
+      l01: {
+        name: "Bahasa",
+        proficiency: "Native"
+      },
+      l02: {
+        name: "English",
+        proficiency: "C1"
+      },
+      l03: {
+        name: "German",
+        proficiency: "B1"
+      }
+    }
+
+  },
+  educationAndTraining: {
+    formalEducation: {
+      bachelor: {
+        institution: "Institut Teknologi Bandung",
+        major: "Electrical Engineering",
+        gpa: "3.0",
+        yearsStart: 2005,
+        yearsEnd: 2011
+      }
+    },
+    academicCourse: {
+
+    },
+    technicalCourse: {
+      tt0000001: {
+
+      }
+    },
+    nonTechnicalCourse: {
+      nt0000001: {
+        name: "Super intensif bahasa Jerman A1 - A2",
+        institution: "Goethe Institut",
+        location: "Bandung",
+        years: 2011
+      },
+      nt0000002: {
+        name: "Super intensif"
+      }
+    },
+  },
+  portfolio: {
+    p0000001: {
+      name: "name",
+      start: "start",
+      end: "end",
+      description: "put the description here",
+      screenshot: "filename",
+      skillSetUsed: [],
+      difficulty: 5
+    }
+  }
+};
