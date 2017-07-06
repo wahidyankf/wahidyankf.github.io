b09297ae05faeb2f4f46f78fd931530044597c5f
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2017-07-06 12:08:31 +0700

    edit data.js information and structure.

--- src/js/data.js
+++ src/js/data.js
@@ -1,70 +1,228 @@
 var dataCV = {
-  biodata: {
+  personaldata: {
     name: "Wahidyan Kresna Fridayoka",
-    nickname: "Yoka",
-    summary: "summary here",
+    shortdesc: "Frontend Web Developer",
+    photos: "img/profile/profilepicture.jpeg",
+    email: "wahidyankf@gmail.com",
+    summary: "I am a frontend web developer, and fairly proficient with HTML, CSS, Sass, and JavaScript.",
+    address: "Jakarta, Indonesia",
+    freeTimeActivities: "Running, Martial Arts, Reading Books, Writing",
+    socialHub: {
+      website: "https://wahidyankf.github.io/",
+      github: "https://github.com/wahidyankf",
+      linkedin: "https://www.linkedin.com/in/wahidyan-kresna-fridayoka",
+      twitter: "https://twitter.com/wahidyankf",
+      teamtreehouse: "https://teamtreehouse.com/wahidyankresnafridayoka",
+      blog: "https://wahidyankf.wordpress.com"
+    },
   },
   skills: {
-    technicalSkills: {
-      proficient: [],
-      fairlyProficient: [],
-      familiar: []
-    },
-    languageSkills: {
-      l01: {
+    language: [{
         name: "Bahasa",
         proficiency: "Native"
       },
-      l02: {
+      {
         name: "English",
-        proficiency: "C1"
+        proficiency: "C1",
+        info: "TOEFL PBT (ETS/AMINEF): 627, IELTS: 7.0"
       },
-      l03: {
+      {
         name: "German",
-        proficiency: "B1"
+        proficiency: "B1",
+        info: "Goethe ZD (B1): Befriedigend (228/300)"
       }
-    }
-
-  },
-  educationAndTraining: {
-    formalEducation: {
-      bachelor: {
-        institution: "Institut Teknologi Bandung",
-        major: "Electrical Engineering",
-        gpa: "3.0",
-        yearsStart: 2005,
-        yearsEnd: 2011
-      }
-    },
-    academicCourse: {
-
-    },
-    technicalCourse: {
-      tt0000001: {
-
-      }
-    },
-    nonTechnicalCourse: {
-      nt0000001: {
-        name: "Super intensif bahasa Jerman A1 - A2",
-        institution: "Goethe Institut",
-        location: "Bandung",
-        years: 2011
+    ],
+    technicalSkills: {
+      keySkills: ["Front End Web Development", "General Programming", "Analytical Thinking"],
+      programmingLanguage: {
+        active: [{
+            name: "JavaScript",
+            rating: 8
+          },
+          {
+            name: "HTML",
+            rating: 9
+          },
+          {
+            name: "CSS",
+            rating: 8
+          }, {
+            name: "Sass",
+            rating: 8
+          }, {
+            name: "Python",
+            rating: 6
+          }
+        ],
+        former: ["Java", "Matlab", "C++", "Assembly (AT Mega 8051), SQL, PHP"]
       },
-      nt0000002: {
-        name: "Super intensif"
-      }
+      toolsAndFrameworks: ["gulp", "markdown", "moment.js", "LaTeX", "vim", "TMUX", "git"],
+      operatingSystem: ["ubuntu", "osx", "windows"]
     },
   },
-  portfolio: {
-    p0000001: {
-      name: "name",
-      start: "start",
-      end: "end",
-      description: "put the description here",
-      screenshot: "filename",
-      skillSetUsed: [],
-      difficulty: 5
+  formalEducation: {
+    bachelor: {
+      institution: "Institut Teknologi Bandung",
+      location: "Bandung, Indonesia",
+      major: "Electrical Engineering",
+      type: "Bachelor",
+      gpa: "3.0",
+      start: "2005/08",
+      end: "2011/06"
     }
-  }
+  },
+  portfolio: [{
+    name: "name",
+    start: "start",
+    end: "end",
+    description: "put the description here",
+    screenshot: "filename",
+    skillSetUsed: [],
+    difficulty: 5
+  }],
+  courses: {
+    engineeringCourse: [{
+        name: "",
+        institution: "",
+        location: "",
+        type: "",
+        description: "",
+        start: "",
+        end: "",
+        grade: ""
+      },
+      {
+        name: "Topics in System Engineering - Test and Fault Tolerance",
+        institution: "University of Paderborn",
+        location: "Paderborn, Germany",
+        type: "University",
+        description: "",
+        start: "2016/04",
+        end: "2016/10",
+        grade: "2.3 (B-)"
+      },
+      {
+        name: "Processing of Semiconductor",
+        institution: "University of Paderborn",
+        location: "Paderborn, Germany",
+        type: "University",
+        description: "",
+        start: "2016/04",
+        end: "2016/10",
+        grade: "3.3 (C-)"
+      },
+      {
+        name: "Project - Energieversorgungsstrukturen der Zukunft",
+        institution: "University of Paderborn",
+        location: "Paderborn, Germany",
+        type: "University",
+        description: "",
+        start: "2014/10",
+        end: "2015/04",
+        grade: "1.0 (A+), best in class"
+      },
+      {
+        name: "Project - Optical Communications and High-Frequency Engineering",
+        institution: "University of Paderborn",
+        location: "Paderborn, Germany",
+        type: "University",
+        description: "",
+        start: "2014/10",
+        end: "2015/04",
+        grade: "2.0 (B)"
+      },
+      {
+        name: "Introduction to Algorithm",
+        institution: "University of Paderborn",
+        location: "Paderborn, Germany",
+        type: "University",
+        description: "",
+        start: "2014/10",
+        end: "2015/04",
+        grade: "1.7 (B+), top 3"
+      }
+    ],
+    nonEngineeringCourse: [{
+        name: "Deutschkurs für Internationale Programmstudierende",
+        institution: "University of Paderborn",
+        location: "Paderborn, Germany",
+        type: "University",
+        description: "German language course for the university students, equal to european language level reference of B2 (upper intermediate).",
+        start: "2015/04",
+        end: "2015/10",
+        grade: "2.3 (B-)"
+      },
+      {
+        name: "Management of Technical Project",
+        institution: "University of Paderborn",
+        location: "Paderborn, Germany",
+        type: "University",
+        description: "",
+        start: "2014/04",
+        end: "2014/10",
+        grade: "1.7 (B+)"
+      },
+      {
+        name: "DSH Vorbereitung Klasse",
+        institution: "RWTH Aachen",
+        location: "Aachen, Germany",
+        type: "Offline",
+        description: "German language preparation class for university entrance exam.",
+        start: "2013/03",
+        end: "2013/06",
+        grade: null
+      },
+      {
+        name: "DSH Vorbereitung Klasse",
+        institution: "VHS Aachen",
+        location: "Aachen, Germany",
+        type: "Offline",
+        description: "German language preparation class for university entrance exam.",
+        start: "2012/10",
+        end: "2013/01",
+        grade: null
+      },
+      {
+        name: "Superintensiv Deutsch B2",
+        institution: "Goethe Institut",
+        location: "Jakarta, Indonesia",
+        type: "Offline",
+        description: "German language class for level B2 (upper intermediate).",
+        start: "2012/01",
+        end: "2012/06",
+        grade: null
+      },
+      {
+        name: "Superintensiv Deutsch A1-B1",
+        institution: "Goethe Institut",
+        location: "Bandung, Indonesia",
+        type: "Offline",
+        description: "German languange class for level A1-B1 (beginner to lower intermediate).",
+        start: "2011/07",
+        end: "2011/12",
+        grade: null
+      },
+    ],
+  },
+  workExperience: [{
+    jobTitle: "Internship",
+    institution: "P2 TELIMEK-LIPI",
+    description: "Responsible for supervisory control documentation of PLT Bayu (wind power plant) 100 KW project.",
+    start: "2011/01",
+    end: "2011/03"
+  }],
+  additionalActivities: [{
+    jobtitle: "",
+    institution: "",
+    description: "",
+    start: "",
+    end: ""
+  },
+  {
+    jobtitle: "",
+    institution: "",
+    description: "",
+    start: "",
+    end: ""
+  }],
 };
