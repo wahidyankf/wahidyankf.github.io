e327f7a6995633124ba76e55349bad934d32768a
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2017-07-06 12:53:32 +0700

    empty the app.js

--- src/js/app.js
+++
@@ -1,6 +0,0 @@
-for (let i = 0; i < dataCV.skills.languageSkills.length; i++){
-  console.log(`language no ${i+1}: `);
-  for (let propName in dataCV.skills.languageSkills[i]){
-    console.log(dataCV.skills.languageSkills[i][propName]);
-  }
-}
