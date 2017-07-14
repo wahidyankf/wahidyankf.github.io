2b1fa0a327fd664f773bfb0bf175fc856bab477c
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2017-07-14 09:46:46 +0700

    update data.js

--- src/js/data.js
+++ src/js/data.js
@@ -4,9 +4,8 @@ var dataCV = {
     shortdesc: "Frontend Web Developer",
     photos: "img/profile/profilepicture.jpeg",
     email: "wahidyankf@gmail.com",
-    summary: "I am a frontend web developer, and fairly proficient with HTML, CSS, Sass, and JavaScript.",
+    summary: "Self-taught JavaScript Programmer with diverse front-end web development portfolio and past experience with PHP and MySQL. Can also code python when necessary. Polyglot that speaks 3+ languages and has more than 3 years experience of living overseas. Has successfully organized nationwide entrepreneurship competition",
     address: "Jakarta, Indonesia",
-    freeTimeActivities: "Running, Martial Arts, Reading Books, Writing",
     socialHub: {
       website: "https://wahidyankf.github.io/",
       github: "https://github.com/wahidyankf",
@@ -17,8 +16,21 @@ var dataCV = {
     }
   },
   skills: {
+    programmingLanguage: [{
+        name: "JavaScript",
+        rating: 7.5
+      },
+      {
+        name: "HTML-CSS-Sass",
+        rating: 8
+      },
+      {
+        name: "Python",
+        rating: 7
+      }
+    ],
     language: [{
-        name: "Bahasa",
+        name: "Bahasa Indonesia",
         proficiency: "Native"
       },
       {
@@ -32,290 +44,218 @@ var dataCV = {
         info: "Goethe ZD (B1): Befriedigend (228/300)"
       }
     ],
-    technicalSkills: {
-      keySkills: ["Front End Web Development", "General Programming", "Analytical Thinking"],
-      programmingLanguage: {
-        active: [{
-            name: "JavaScript",
-            rating: 8
-          },
-          {
-            name: "HTML",
-            rating: 9
-          },
-          {
-            name: "CSS",
-            rating: 8
-          }, {
-            name: "Sass",
-            rating: 8
-          }, {
-            name: "Python",
-            rating: 6
-          }
-        ],
-        former: ["Java", "Matlab", "C++", "Assembly (AT Mega 8051), SQL, PHP"]
-      },
-      toolsAndFrameworks: ["gulp", "markdown", "moment.js", "LaTeX", "vim", "TMUX", "git"],
-      operatingSystem: ["ubuntu", "osx", "windows"]
-    }
+    toolsAndLibraries: ["gulp.js", "markdown", "moment.js", "LaTeX", "vim", "TMUX", "git", "CSS post processing", "foundation for sites"],
+    pastExperience: ["Matlab", "C++", "Assembly (AT Mega 8051)", "SQL", "PHP", "Gnokii SMS Daemon"],
+    operatingSystem: ["Ubuntu", "Mac", "Windows"]
   },
-  formalEducation: {
-    bachelor: {
-      institution: "Institut Teknologi Bandung",
-      location: "Bandung, Indonesia",
-      major: "Electrical Engineering",
-      type: "Bachelor",
-      gpa: "3.0",
-      start: "2005/08",
-      end: "2011/06"
-    }
-  },
+  formalEducation: [{
+    major: "Electrical Engineering",
+    type: "Bachelor",
+    institution: "Institut Teknologi Bandung",
+    start: "08/2005",
+    end: "06/2011",
+    info: "GPA: 3.0/4.0. Bachelor thesis: Design and implementation of SMS One Time Password (SMS-OTP) Based Web Authentication System. Supervisor: Ir. Budi Rahardjo M.Sc., Ph.D."
+  }],
   portfolio: [{
-      name: "",
-      start: "",
-      end: "",
-      description: " the description here",
-      image: "",
-      skillSetUsed: [],
-      difficulty: "",
-      link: ""
+      name: "wahidyankf.github.io",
+      description: "In this project, I created a portfolio website for my personal used. This website contains the complete list of my curriculum vitae.",
+      skillSetUsed: ["HTML", "CSS", "Sass", "JavaScript", "jQuery"],
+      start: "07/2017",
+      end: "07/2017",
+      link: "https://wahidyankf.github.io/"
     },
     {
-      name: "",
-      start: "",
-      end: "",
-      description: " the description here",
-      image: "",
-      skillSetUsed: [],
-      difficulty: "",
-      link: ""
+      name: "SVG Site Update",
+      description: "In this project, a functioning website was given to me, and I have to update/replace its existing non svg images with svg images, resulting more beautiful and sharper images on the website.",
+      skillSetUsed: ["HTML", "CSS", "Sass"],
+      start: "06/2017",
+      end: "06/2017",
+      link: "https://github.com/wahidyankf/treehouse-frontend-06-svg-site-update"
     },
     {
-      name: "",
-      start: "",
-      end: "",
-      description: " the description here",
-      image: "",
-      skillSetUsed: [],
-      difficulty: "",
-      link: ""
+      name: "Moslem Productivity Tools",
+      description: "In this project, I created a simple website application that tracks the islamic praying time and section of the day's markers, and warns me whenever I am about to burnout (according to the scientific theory of productivity). This app allow me to always work at my optimal speed.",
+      skillSetUsed: ["HTML", "CSS", "Sass", "JavaScript", "jQuery", "Foundation for Sites", "moment.js", "praytime.js"],
+      start: "04/2017",
+      end: "04/2017",
+      link: "https://github.com/wahidyankf/moslem-productivity-tools"
     },
     {
-      name: "",
-      start: "",
-      end: "",
-      description: " the description here",
-      image: "",
-      skillSetUsed: [],
-      difficulty: "",
-      link: ""
+      name: "Interactive Photo Gallery",
+      description: "In this project, an interactive online gallery's design mockup was given to me, and I have to replicate it using only jQuery as a non vanilla js element. This project includes a simple lightbox (with navigation) and search/filter function.",
+      skillSetUsed: ["HMTL", "CSS", "Sass", "JavaScript", "jQuery"],
+      start: "04/2017",
+      end: "04/2017",
+      link: "https://github.com/wahidyankf/treehouse-frontend-04-interactive-photo-gallery"
     },
     {
-      name: "",
-      start: "",
-      end: "",
-      description: " the description here",
-      image: "",
-      skillSetUsed: [],
-      difficulty: "",
-      link: ""
+      name: "Online Registration Form",
+      description: "In this project, a responsive online form's design mockup was given to me, and I have to replicate it using vanilla HTML and CSS.",
+      skillSetUsed: ["HTML", "CSS", "Sass"],
+      start: "04/2017",
+      end: "04/2017",
+      link: "https://github.com/wahidyankf/treehouse-frontend-03-online-registration-form"
     },
     {
-      name: "",
-      start: "",
-      end: "",
-      description: " the description here",
-      image: "",
-      skillSetUsed: [],
-      difficulty: "",
-      link: ""
+      name: "Responsive Layout",
+      description: "In this project, a responsive web portfolio mockup was given to me, and I have to replicate it using HTML and CSS.",
+      skillSetUsed: ["HTML", "CSS", "Sass"],
+      start: "04/2017",
+      end: "04/2017",
+      link: "https://github.com/wahidyankf/treehouse-frontend-02-responsive-layout"
     },
     {
-      name: "",
-      start: "",
-      end: "",
-      description: " the description here",
-      image: "",
-      skillSetUsed: [],
-      difficulty: "",
-      link: ""
+      name: "Personal Profile Page",
+      description: "In this project, I modified an existing personal website to conform to my personal data.",
+      skillSetUsed: ["HTML", "CSS"],
+      start: "04/2017",
+      end: "04/2017",
+      link: "https://github.com/wahidyankf/treehouse-frontend-01-personal-profile-page"
     },
     {
-      name: "",
-      start: "",
-      end: "",
-      description: " the description here",
-      image: "",
-      skillSetUsed: [],
-      difficulty: "",
-      link: ""
+      name: "Build a Portfolio Site",
+      description: "In this project, I created a simple responsive portfolio site using foundation for sites.",
+      skillSetUsed: ["HTML", "CSS", "Foundation for Sites"],
+      start: "03/2017",
+      end: "03/2017",
+      link: "https://github.com/wahidyankf/udacity-fullstack-build-a-portfolio-site"
     },
     {
-      name: "",
-      start: "",
-      end: "",
-      description: " the description here",
-      image: "",
-      skillSetUsed: [],
-      difficulty: "",
-      link: ""
-    },
-    {
-      name: "",
-      start: "",
-      end: "",
-      description: " the description here",
-      image: "",
-      skillSetUsed: [],
-      difficulty: "",
-      link: ""
+      name: "Movie Trailer Website",
+      description: "In this project, I created a movie trailer website using python",
+      skillSetUsed: ["Python"],
+      start: "03/2017",
+      end: "03/2017",
+      link: "https://github.com/wahidyankf/udacity-fullstack-movie-trailer-website"
     }
   ],
   courses: {
     engineeringCourse: [{
-        name: "",
-        institution: "",
-        location: "",
-        type: "",
-        description: "",
-        start: "",
-        end: "",
-        grade: ""
-      },
-      {
         name: "Topics in System Engineering - Test and Fault Tolerance",
         institution: "University of Paderborn",
         location: "Paderborn, Germany",
-        type: "University",
-        description: "",
-        start: "2016/04",
-        end: "2016/10",
-        grade: "2.3 (B-)"
+        description: "Grade: 2.3 (B-). In this course a series of student seminar were given on the subject of test and fault tolerance of an IC system. In this course, I wrote and present a paper about artificial neural network.",
+        start: "04/2016",
+        end: "10/2016",
       },
       {
         name: "Processing of Semiconductor",
         institution: "University of Paderborn",
         location: "Paderborn, Germany",
-        type: "University",
-        description: "",
-        start: "2016/04",
-        end: "2016/10",
-        grade: "3.3 (C-)"
+        description: "Grade: 3.3 (C-). This lecture describes the technical steps to build integrated circuits on silicon wafers. Physical models of the process steps were given and the technical equipment were explained.",
+        start: "04/2016",
+        end: "10/2016",
       },
       {
         name: "Project - Energieversorgungsstrukturen der Zukunft",
         institution: "University of Paderborn",
         location: "Paderborn, Germany",
-        type: "University",
-        description: "",
-        start: "2014/10",
-        end: "2015/04",
-        grade: "1.0 (A+), best in class"
+        description: "Grade: 1.0 (A+), best in class. This course gives an introduction to the energy structure of the future. In this course, I wrote a report about the impact of the usage of electric vehicles on the future and current energy structure.",
+        start: "10/2014",
+        end: "04/2015",
       },
       {
         name: "Project - Optical Communications and High-Frequency Engineering",
         institution: "University of Paderborn",
         location: "Paderborn, Germany",
-        type: "University",
-        description: "",
-        start: "2014/10",
-        end: "2015/04",
-        grade: "2.0 (B)"
+        description: "Grade: 2.0 (B). A series of laboratory experiments and simulation of optical communications and high frequency engineering were held in this course.",
+        start: "10/2014",
+        end: "04/2015",
       },
       {
         name: "Introduction to Algorithm",
         institution: "University of Paderborn",
         location: "Paderborn, Germany",
-        type: "University",
-        description: "",
-        start: "2014/10",
-        end: "2015/04",
-        grade: "1.7 (B+), top 3"
+        description: "Grade: 1.7 (B+), top 3 in class. This course gives an introduction into the design and analysis of algorithms (sorting algorithms, basic data structures, graphs and graph algorithms, design and analysis of algorithms (problem complexity, run time and storage complexity of algorithms, exact vs heuristic solutions, probabilistic approaches)).",
+        start: "10/2014",
+        end: "04/2015",
       }
     ],
     nonEngineeringCourse: [{
         name: "Deutschkurs für Internationale Programmstudierende",
         institution: "University of Paderborn",
         location: "Paderborn, Germany",
-        type: "University",
-        description: "German language course for the university students, equal to european language level reference of B2 (upper intermediate).",
-        start: "2015/04",
-        end: "2015/10",
-        grade: "2.3 (B-)"
+        description: "Grade: 2.3 (B-). German language course for the university students, equal to european language level reference of B2 (upper intermediate).",
+        start: "04/2015",
+        end: "10/2015",
       },
       {
         name: "Management of Technical Project",
         institution: "University of Paderborn",
         location: "Paderborn, Germany",
-        type: "University",
-        description: "",
-        start: "2014/04",
-        end: "2014/10",
-        grade: "1.7 (B+)"
+        description: "Grade: 1.7 (B+). In this course, students were taught about soft skills on how to manage technical projects (e.g. requirement analysis, specification, scheduling, planning & designing, monitoring & controlling, communication in teams, communication with customers).",
+        start: "04/2014",
+        end: "10/2014",
       },
       {
         name: "DSH Vorbereitung Klasse",
         institution: "RWTH Aachen",
         location: "Aachen, Germany",
-        type: "Offline",
         description: "German language preparation class for university entrance exam.",
-        start: "2013/03",
-        end: "2013/06",
-        grade: null
+        start: "03/2013",
+        end: "06/2013",
       },
       {
         name: "DSH Vorbereitung Klasse",
         institution: "VHS Aachen",
         location: "Aachen, Germany",
-        type: "Offline",
         description: "German language preparation class for university entrance exam.",
-        start: "2012/10",
-        end: "2013/01",
-        grade: null
+        start: "10/2012",
+        end: "01/2013",
       },
       {
         name: "Superintensiv Deutsch B2",
         institution: "Goethe Institut",
         location: "Jakarta, Indonesia",
-        type: "Offline",
         description: "German language class for level B2 (upper intermediate).",
-        start: "2012/01",
-        end: "2012/06",
-        grade: null
+        start: "01/2012",
+        end: "06/2012",
       },
       {
         name: "Superintensiv Deutsch A1-B1",
         institution: "Goethe Institut",
         location: "Bandung, Indonesia",
-        type: "Offline",
         description: "German languange class for level A1-B1 (beginner to lower intermediate).",
-        start: "2011/07",
-        end: "2011/12",
-        grade: null
+        start: "07/2011",
+        end: "12/2011",
       }
     ]
   },
   workExperience: [{
     jobTitle: "Internship",
     institution: "P2 TELIMEK-LIPI",
-    description: "Responsible for supervisory control documentation of PLT Bayu (wind power plant) 100 KW project.",
-    start: "2011/01",
-    end: "2011/03"
+    description: "Taking part on the documentation of PLT Bayu (wind power plant) 100 KW project's supervisory control.",
+    start: "01/2011",
+    end: "03/2011"
   }],
   additionalActivities: [{
-      jobtitle: "",
-      institution: "",
-      description: "",
-      start: "",
-      end: ""
+      jobtitle: "Freelance Writer/Moderator",
+      institution: "media.kelasnusantara.com",
+      description: "Writing articles and moderating a blog about education and learning in general. Best month's view average: 575 views/day.",
+      start: "12/2013",
+      end: "12/2015"
     },
     {
-      jobtitle: "",
-      institution: "",
-      description: "",
-      start: "",
-      end: ""
+      jobtitle: "Freelance Writer/Moderator",
+      institution: "jermandes.kelasnusantara.com",
+      description: "Writing articles and moderating a blog about how to prepare for studying in Germany. During that time, this site was one of the pioneers in its niche and always shown on google search's first page. Best month's view average: 609 views/day.",
+      start: "12/2011",
+      end: "12/2013"
+    }
+  ],
+  organizationalExperience: [{
+      jobtitle: "Head of Logistics Division",
+      institution: "Innovative Entrepreneurship Challenge 3 (IEC 3)",
+      location: "Bandung, Indonesia",
+      description: "I successfully secured the logistic matters of Institut Teknologi Bandung's annual national scale entrepreneurship competition, resulting a smooth run of the competition.",
+      start: "2009",
+      end: "2009"
+    },
+    {
+      jobtitle: "Head of Public Relation Division",
+      institution: "Palapa 1, ITB's Electrical Engineering's Student Org.",
+      location: "Bandung, Indonesia",
+      description: "An Insititut Teknologi Bandung's student organization's charity project. I successfully managed the relationship between the student organization and the targeting area's native. Resulting a smooth process of pico hydro power plant installation in a relatively remote area in Garut (west java, Indonesia."
     }
   ]
 };
