44cad70e73ea2da3ea771e3a4c5fd9a1cab7881b
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2017-07-19 09:26:03 +0700

    finish main content loader

---
+++ src/js/app.js
@@ -0,0 +1,12 @@
+var mainContent = `
+${summaryContent}
+${skillsContent}
+${portfolioContent}
+${educationContent}
+${workExperienceContent}
+${additionalActivitiesContent}
+${organizationalExperienceContent}
+`;
+
+$('header').append(headerContent);
+$('main').append(mainContent);
