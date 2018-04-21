e5b512193e0a90293080ccdc6962d7706dca7d89
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2018-04-21 15:12:35 +0700

    update 04/21/18 15:12:35

--- src/js/data.js
+++ src/js/data.js
@@ -4,7 +4,7 @@ const dataCV = {
     shortDesc: 'Front-End / Software Engineer',
     photos: 'img/profile/profilepicture.jpeg',
     email: 'wahidyankf@gmail.com',
-    summary: `Software engineer with diverse front-end web development portfolio. Comfortable working with JavaScript, React and React Native stacks but also eager to learn another programming language and tools. Have exposure to Node.js and Python. Polyglot that speaks 3+ languages and has more than 3 years experience of living overseas. Has successfully organized nationwide entrepreneurship competition.`,
+    summary: `Software engineer with diverse front-end web development's portfolio. Comfortable working with JavaScript, React and React Native stacks but also eager to learn another programming language and tools. Have exposure to Node.js and Python. Polyglot that speaks 3+ languages and has more than 3 years experience of living overseas. Has successfully organized nationwide entrepreneurship competition.`,
     address: 'Jakarta, Indonesia',
     socialHub: {
       website: 'https://wahidyankf.github.io/',
