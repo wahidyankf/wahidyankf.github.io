5474288065bcfd114707aac353254bddede748a1
wahidyankf <wahidyankf@gmail.com>
2019-04-20 05:27:34 +0700

    code refactor

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -8,12 +8,6 @@ import {
 } from './cvbuilder';
 import '../css/app.css';
 
-const jsCheckEl = document.getElementById('js-check');
-
-if (jsCheckEl) {
-  jsCheckEl.style.display = 'none';
-}
-
 const main = `
 ${summary}
 ${workExperience}
@@ -22,13 +16,17 @@ ${education}
 ${portfolio}
 `;
 
-const headerEl = document.getElementById('header');
-const mainEl = document.getElementById('main');
+const jsCheckEl = document.getElementById('js-check');
+if (jsCheckEl) {
+  jsCheckEl.style.display = 'none';
+}
 
+const headerEl = document.getElementById('header');
 if (headerEl) {
   headerEl.innerHTML = header;
 }
 
+const mainEl = document.getElementById('main');
 if (mainEl) {
   mainEl.innerHTML = main;
 }
