cb96a7d1229f98d560c206ab793540073324d8ec
wahidyankf <wahidyankf@gmail.com>
2019-04-19 19:24:08 +0700

    change parcel config

--- src/js/app.js
+++ src/js/app.js
@@ -25,12 +25,3 @@ ${organizationalExperienceContent}
 
 $('header').append(headerContent);
 $('main').append(mainContent);
-
-if (module.hot) {
-  module.hot.dispose(function () {
-    // module is about to be replaced
-  });
-  module.hot.accept(function () {
-    // module or one of its dependencies was just updated
-  });
-}
